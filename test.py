be5fbb687f89c10e2bafa5d39efac5a4cf13da61
Twice_Ice <125768114+Twice-Ice@users.noreply.github.com>
2024-08-23 17:06:48 -0600

    removed test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-while True:
-    num = random.randint(0, 1)
-    num = num if num > 0 else -1
-    print((num * 15) + (random.randint(0, 15) if num > 0 else random.randint(-15, 0)))
